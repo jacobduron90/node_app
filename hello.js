ea475282cdf609b4e0f59074430ca3b7a48227a3
jacobduron90 <jacobduron@gmail.com>
2016-08-11 18:20:26 -0500

    adding static data

--- hello.js
+++ hello.js
@@ -1,7 +1,19 @@
 var http = require('http');
 var APP_PRIVATE_IP_ADDRESS = '104.131.83.239'
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8080, APP_PRIVATE_IP_ADDRESS);
+var express = require('express');
+var app = express();
+
+
+app.get("/", function(req,res){
+	res.send("OK, got it");
+});
+
+app.use(express.static('public'));
+
+var port = process.env.PORT || 8081;
+ app.listen(port, function() {
+   console.log("Listening on " + port);
+ });
+
+
 console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8080/');
